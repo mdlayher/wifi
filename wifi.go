94089997dc0c2c88c0265d424fa8ef6779bd401e
Lukas Raffelt <151549830+lukas-mbag@users.noreply.github.com>
2025-10-30 09:42:52 +0100

    Update wifi.go
    Co-authored-by: Ben Kochie <superq@gmail.com>

--- wifi.go
+++ wifi.go
@@ -248,10 +248,10 @@ type BSS struct {
 	Status BSSStatus
 
 	// Signal: The signal strength of the BSS, in mBm (divide by 100 to get dBm).
-	Signal int
+	Signal int32
 
 	// SignalUnspecified: The signal strength of the BSS, in percent.
-	SignalUnspecified int
+	SignalUnspecified uint32
 
 	// Load: The load element of the BSS (contains StationCount, ChannelUtilization and AvailableAdmissionCapacity).
 	Load BSSLoad
